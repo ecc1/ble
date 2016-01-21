2d6cc1db403e17ba089ca17c76d36ca7bdfacb14
Eric Cooper <ecc@cmu.edu>
2016-01-20 22:20:12 -0500

    Add GATT functions

---
+++ gatt.go
@@ -0,0 +1,75 @@
+package ble
+
+const (
+	serviceInterface        = "org.bluez.GattService1"
+	characteristicInterface = "org.bluez.GattCharacteristic1"
+	descriptorInterface     = "org.bluez.GattDescriptor1"
+)
+
+type gattHandle interface {
+	base
+
+	UUID() string
+}
+
+func (handle *blob) UUID() string {
+	return handle.properties["UUID"].Value().(string)
+}
+
+type Service interface {
+	gattHandle
+}
+
+func (cache *ObjectCache) GetService(uuid string) (Service, error) {
+	return cache.find(serviceInterface, func(serv *blob) bool {
+		return serv.UUID() == uuid
+	})
+}
+
+type readWriteHandle interface {
+	gattHandle
+
+	ReadValue() ([]byte, error)
+	WriteValue([]byte) error
+}
+
+func (handle *blob) ReadValue() ([]byte, error) {
+	var data []byte
+	err := handle.callv("ReadValue").Store(&data)
+	return data, err
+}
+
+func (handle *blob) WriteValue(data []byte) error {
+	return handle.call("WriteValue", data)
+}
+
+type Characteristic interface {
+	readWriteHandle
+
+	StartNotify() error
+	StopNotify() error
+}
+
+func (cache *ObjectCache) GetCharacteristic(uuid string) (Characteristic, error) {
+	return cache.find(characteristicInterface, func(char *blob) bool {
+		return char.UUID() == uuid
+	})
+}
+
+func (char *blob) StartNotify() error {
+	return char.call("StartNotify")
+}
+
+func (char *blob) StopNotify() error {
+	return char.call("StopNotify")
+}
+
+type Descriptor interface {
+	readWriteHandle
+}
+
+func (cache *ObjectCache) GetDescriptor(uuid string) (Descriptor, error) {
+	return cache.find(descriptorInterface, func(desc *blob) bool {
+		return desc.UUID() == uuid
+	})
+}
