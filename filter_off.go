a00e7e5b0f6f01b0a9430766beabdcf2e54c3d29
Eric Cooper <ecc@cmu.edu>
2016-02-18 14:45:51 -0500

    Fix typo

--- filter_off.go
+++ filter_off.go
@@ -2,8 +2,8 @@
 
 package ble
 
-// Discvoery filtering doesn't work on Intel Edison
-// running Debian stretch and kernel 3.10.17-yocto-standard-r2.
+// Discovery filtering doesn't work on Intel Edison running
+// Debian stretch and kernel 3.10.17-yocto-standard-r2.
 func (adapter *blob) SetDiscoveryFilter(uuids ...string) error {
 	return nil
 }
